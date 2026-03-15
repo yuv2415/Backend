357469a2d613eb1fac63b62e2ab728665a289165
Rohit Rajput <yuvir3236@gmail.com>
2026-03-15 22:06:27 +0530

    learn create server

--- practice/index.js
+++ practice/index.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+require('dotenv').config();
+const express=require('express');
+const app=express();
+const PORT=3000;
+
+app.get('/',(req,res)=>{
+    res.send("Server is running");
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+
+
+
+
+
+
+
